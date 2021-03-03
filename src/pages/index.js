cabb6a12d5056ea9a045f0637d2d00e17c35d381
Otto Kaaij <o.k.n.kaaij@student.tudelft.nl>
2021-03-03 21:50:43 +0100

    Finish template for Tijs

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,28 @@
 import React from "react"
+import Img from "gatsby-image"
 
-export default function Home() {
-  return <div className="main">Website hier, Thijs, ga bouwen.</div>
-
+export default function Home({ data }) {
+  return (
+    <div className="main" style={{ display: "flex", flexDirection: "column" }}>
+      Website hier, Tijs, ga bouwen.
+      <Img
+        style={{ marginTop: "1em" }}
+        fixed={data.file.childImageSharp.fixed}
+      />
+    </div>
+  )
 }
+
+export const query = graphql`
+  query {
+    file(relativePath: { eq: "image.jpg" }) {
+      childImageSharp {
+        # Specify the image processing specifications right in the query.
+        # Makes it trivial to update as your page's design changes.
+        fixed(width: 485, height: 760) {
+          ...GatsbyImageSharpFixed
+        }
+      }
+    }
+  }
+`
